e7f0fb42d538bae66164c11821ee354143eae180
Alison Stripeback <astripeb@mi-t3.21-school.ru>
2019-04-12 21:42:17 +0300

    add fix atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,15 +6,19 @@
 /*   By: astripeb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 19:46:43 by astripeb          #+#    #+#             */
-/*   Updated: 2019/04/12 19:49:43 by astripeb         ###   ########.fr       */
+/*   Updated: 2019/04/12 21:25:08 by astripeb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int					ft_atoi(const char *str)
 {
-	int		nb;
-	int		flag;
+	int			nb;
+	int			flag;
+	long long	lnbr;
 
+	lnbr = 0;
 	nb = 0;
 	flag = 1;
 	while ((*str > 8 && *str < 14) || *str == 32)
@@ -23,9 +27,12 @@ int					ft_atoi(const char *str)
 		flag = 44 - *str++;
 	while (*str > 47 && *str < 58)
 	{
-		nb += *str++ - 48;
-		if (*str > 47 && *str < 58)
-			nb *= 10;
+		lnbr = lnbr * 10 + (*str - 48);
+		nb = nb * 10 + (*str++ - 48);
 	}
+	if (lnbr < 0 && flag == -1)
+		return (0);
+	else if (lnbr < 0 && flag == 1)
+		return (-1);
 	return (nb * flag);
 }

cceb14a6a6510628e28a4fe6b8ae317735a19d98
Alison Stripeback <astripeb@mi-t3.21-school.ru>
2019-04-12 19:53:29 +0300

    add excelent ft_adoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,53 +6,26 @@
 /*   By: astripeb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 19:46:43 by astripeb          #+#    #+#             */
-/*   Updated: 2019/04/09 19:23:00 by astripeb         ###   ########.fr       */
+/*   Updated: 2019/04/12 19:49:43 by astripeb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int			negative(const char *str, int i)
-{
-	if (str[i] == '-' && (str[i + 1] > 47 && str[i + 1] < 58))
-		return (-1);
-	else if (str[i] == '+' && (str[i + 1] > 47 && str[i + 1] < 58))
-		return (1);
-	return (0);
-}
-
-static int			tabs(const char *str)
-{
-	int i;
-
-	i = 0;
-	while ((str[i] > 8 && str[i] < 14) || str[i] == 32)
-		++i;
-	return (i);
-}
-
 int					ft_atoi(const char *str)
 {
-	int			i;
-	int			nb;
-	int			flag;
+	int		nb;
+	int		flag;
 
 	nb = 0;
-	i = tabs(str);
-	flag = negative(str, i);
-	if (flag == 1 || flag == -1)
-		++i;
-	while (str[i] != '\0')
+	flag = 1;
+	while ((*str > 8 && *str < 14) || *str == 32)
+		++str;
+	if (*str == '-' || *str == '+')
+		flag = 44 - *str++;
+	while (*str > 47 && *str < 58)
 	{
-		if (str[i] > 47 && str[i] < 58)
-		{
-			nb += str[i] - 48;
-			if (str[i + 1] > 47 && str[i + 1] < 58)
-				nb *= 10;
-		}
-		else
-			break ;
-		++i;
+		nb += *str++ - 48;
+		if (*str > 47 && *str < 58)
+			nb *= 10;
 	}
-	if (flag == -1)
-		nb *= flag;
-	return (nb);
+	return (nb * flag);
 }

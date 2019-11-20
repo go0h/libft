13de2a287d661878270730d983e2d70548c97c75
go0h <aks64@yandex.ru>
2019-11-20 15:17:36 +0300

    remove from atoi overflow

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pcredibl <pcredibl@student.42.fr>          +#+  +:+       +#+        */
+/*   By: astripeb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 19:46:43 by astripeb          #+#    #+#             */
-/*   Updated: 2019/08/28 12:29:21 by pcredibl         ###   ########.fr       */
+/*   Updated: 2019/11/20 15:16:51 by astripeb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,7 @@ int					ft_atoi(const char *str)
 		++str;
 	flag = (*str == '+' || *str == '-') ? (44 - *str++) : 1;
 	while (*str > 47 && *str < 58)
-	{
-		if ((nb > 214748363 && *str > '7' && flag == 1) ||\
-				nb > 214748364)
-			return (-1);
-		else if ((nb > 214748363 && *str > '8' && flag == -1) ||\
-				nb > 214748364)
-			return (0);
 		nb = nb * 10 + (*str++ - '0');
-	}
 	return (nb * flag);
 }
 
